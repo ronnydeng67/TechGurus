41f770bd593a51950a0cae69adacd17adf3050d6
Ronny Deng <ronnydeng67@gmail.com>
2022-10-07 16:46:37 -0400

    done user model

---
+++ backend/app/models/user.rb
@@ -0,0 +1,31 @@
+class User < ApplicationRecord
+    validates :name, length: {in: 3..30 }, format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { in: 6..255 }, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+       user = User.find_by(email)
+       user&.authenticate(password)
+    end
+
+    def reset_session_token!
+        self.update!(session_token: genertare_token)
+        self.session_token
+    end
+
+    private
+
+    def genertare_token
+        loop do
+            token = SecureRandom.base64
+            break token unless User.exists?(session_token: token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= genertare_token
+    end
+end
