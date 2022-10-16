a65c907545e8dfb4ec97975e32882c1e09f8c08f
Ronny Deng <ronnydeng67@gmail.com>
2022-10-16 17:41:57 -0400

    splash skeleton done

--- frontend/src/component/Splash/Splash.js
+++ frontend/src/component/Splash/Splash.js
@@ -1,5 +1,7 @@
+import { fontSize } from '@mui/system';
 import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchItems } from '../../store/items';
 import './Splash.css';
 
@@ -11,10 +13,15 @@ const Splash = () => {
     useEffect(() => {
         dispatch(fetchItems()).then((res)=> {
             setItems(Object.values(res))
-        }).finally(()=> {
+        }).then(()=> {
             setIsLoading(false)
         })
     }, [])
+
+    const handleClearance = (e) => {
+        e.preventDefault();
+
+    }
 
     if(isLoading){
         return <div>Loading....</div> 
@@ -37,28 +44,55 @@ const Splash = () => {
                             </div>
                             <div className="today-picks">
                                 <div className="pick-1">
-                                    {items[0].name}
+                                    <Link>{items[0].name}</Link>
                                 </div>
                                 <div className="pick-2">
-                                    {items[1].name}
+                                    <Link>{items[1].name}</Link>
                                 </div>
                                 <div className="pick-3">
-                                    {items[2].name}
+                                    <Link>{items[2].name}</Link>
                                 </div>
                                 <div className="pick-4">
-                                    {items[3].name}
+                                    <Link>{items[3].name}</Link>
                                 </div>
                             </div>
                         </div>
                         <div className="top-right-bottom">
                             <div className="outlet-container">
                                 <div className="outlet">
-    
+                                    <div className="button-box">
+                                        <button id='outlet'>Outlet</button>
+                                        <button id='deal'>Deals</button>
+                                    </div>
+                                    <div className="below-button" onClick={handleClearance}>
+                                        <div className="clearance" style={{color: "#bb0628", fontSize: '3.6rem', textAlign: 'center', marginTop: "50px", fontFamily: 'bestbuy-medium'}}>
+                                            Clearance,
+                                        </div>
+                                        <div className="open-box" style={{color: "black", fontSize: '3.6rem', textAlign: 'center', fontFamily: 'bestbuy-medium'}}>
+                                            open-box and more.
+                                        </div>
+                                    </div>
+                                        <div className="outlet-details">
+                                            <Link style={{color: "#0346be", fontSize: "medium"}}>View Outlet deals</Link>
+                                        </div>
                                 </div>
                             </div>
                             <div className="deal-of-day-container">
                                 <div className="deal-of-day">
-    
+                                    <div className="deal-title">
+                                        <div className="deal-text">
+                                            Deal
+                                        </div>
+                                        <div className="deal-rest-title">
+                                            of the day
+                                        </div>
+                                    </div>
+                                    <div className="deal-item">
+                                        {items[4].name}
+                                    </div>
+                                    <div className="bonus-deals">
+                                        <Link style={{color: "#0346be", fontSize: 'medium'}}>See bonus deals</Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
