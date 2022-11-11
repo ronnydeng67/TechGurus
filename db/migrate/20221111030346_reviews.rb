8628318c04899be94caa7235d9af2af37f48af6e
Ronny Deng <ronnydeng67@gmail.com>
2022-11-10 22:28:04 -0500

    migrate review

---
+++ db/migrate/20221111030346_reviews.rb
@@ -0,0 +1,12 @@
+class Reviews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reviews do |t|
+      t.references :reviewer, null: false, foreign_key: {to_table: :users}
+      t.references :item, null: false, foreign_key: {to_table: :items}
+      t.string :title, null: false
+      t.text :body, null: false
+      t.float :rating, null: false
+      t.timestamps
+    end
+  end
+end
