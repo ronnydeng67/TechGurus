ed720a247caa0267e147ea87eb61da888b521a63
Ronny Deng <ronnydeng67@gmail.com>
2022-11-11 14:18:59 -0500

    finish controller

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,3 +1,43 @@
 class Api::ItemsController < ApplicationController
 
+    def index
+        item = Review.find_by(id: params[:item_id])
+        @reviews = item.reviews
+        render :index
+    end
+
+    def create
+        @review = Review.new(review_params)
+        if @review.save
+            render :show
+        else
+            render json: {errors: @review.errors.full_messages}, status: 422
+    end
+
+    def destroy
+        @reivew = Review.find(params[:id])
+        if @review
+            @review.destroy
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
+    end
+
+    def update 
+        @review = Review.find_by(id: params[:id])
+        if @review.update(review_params)
+            render: show
+        else
+            render json: @review.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @review = Review.find_by(id: parms[:id])
+        render :show
+    end
+
+    def review_params
+        params.require(:review).permit(:title, :body, :rating, :reviewer_id, :item_id)
+    end
 end
