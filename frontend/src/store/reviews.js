77c2ab60a4129f33c68c7d43e456d03bd7ff433c
Ronny Deng <ronnydeng67@gmail.com>
2022-11-12 01:08:13 -0500

    add fetch reviews thunk zzzzz

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,5 +1,31 @@
+import csrfFetch from "./csrfFetch";
+
 export const RECEIVE_REVIEW = '/RECEVIE_REVIEW';
 export const RECEIVE_REVIEWS = '/RECEVIE_REVIEWS';
 export const CREATE_REVIEW = '/CREATE_REVIEW';
 export const UPDATE_REVIEW = '/UPDATE_REVIEW';
-export const DELETE_REVIEW = '/DELETE_REVIEW';
+export const REMOVE_REVIEW = '/REMOVE_REVIEW';
+
+const receiveReviews = reviews => ({
+    type: RECEIVE_REVIEWS,
+    reviews
+})
+
+const receiveReview = review => ({
+    type: RECEIVE_REVIEW,
+    review
+})
+
+const removeReview = reviewId => ({
+    type: REMOVE_REVIEW,
+    reviewId
+})
+
+export const getReview = reviewId => ({ reviews }) => (reviews ? reviews[reviewId] : null);
+export const getReviews = ({ reviews }) => (reviews ? Object.values(reviews): []);
+
+export const fetchReviews = () => async dispatch => {
+    const res = await csrfFetch('/api/reviews');
+    const data = await res.json();
+    dispatch(receiveReviews(data));
+}
