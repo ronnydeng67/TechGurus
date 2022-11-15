15fa346fe9637a39d165b1009f8c8581afc70142
Ronny Deng <ronnydeng67@gmail.com>
2022-11-15 18:57:13 -0500

    add style to search page

--- frontend/src/component/SearchResultPage/SearchResults.js
+++ frontend/src/component/SearchResultPage/SearchResults.js
@@ -1,53 +1,36 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { fetchReviews, getReviews } from "../../store/reviews";
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 const SearchResults = ({ resultItem }) => {
     
-    const dispatch = useDispatch();
-    const [loaded, setLoaded] = useState(false)
-    const reviews = useSelector(getReviews);
 
-    useEffect(() => {
-        dispatch(fetchReviews(resultItem.item.id)).then(() => (
-            setLoaded(true)
-        ))
-    },[])
-
-    console.log(reviews)
-
-    if(loaded) {
-        return (
-            <div className="result-item-container">
-                <div className="result-pic-container">
+    return (
+        <div className="result-item-container">
+            <div className="result-pic-container">
+                <Link to={`/items/${resultItem.item.id}`}>
+                    <img src={resultItem.item.photoUrl} alt="" id="result-pic"/>
+                </Link>
+            </div>
+            <div className="result-info-container">
+                <div className="result-name">
                     <Link to={`/items/${resultItem.item.id}`}>
-                        <img src={resultItem.item.photoUrl} alt="" id="result-pic"/>
+                        {resultItem.item.name}
                     </Link>
                 </div>
-                <div className="result-info-container">
-                    <div className="result-name">
-                        <Link to={`/items/${resultItem.item.id}`}>
-                            {resultItem.item.name}
-                        </Link>
-                    </div>
-                    <div className="total-reviews">
-                        {reviews?.length}
-                    </div>
+            </div>
+            <div className="result-price-container">
+                <div className="result-price">
+                    ${resultItem.item.price}
                 </div>
-                <div className="result-price-container">
-        
+                <div className="result-add-cart">
+                    <button id='result-add-cart'>
+                        <ShoppingCartIcon style={{marginRight: "10px", fontSize: "medium"}}/>
+                        Add to Cart
+                    </button>
                 </div>
             </div>
-        );
-    } else {
-        return (
-            <div>
-                Loading...
-            </div>
-        )
-    }
+        </div>
+    );
 }
  
 export default SearchResults;
