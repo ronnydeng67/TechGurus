08dbaa50457a0641aa615bc2291fdccab994264b
Ronny Deng <ronnydeng67@gmail.com>
2023-04-03 20:26:33 -0400

    add more boxes for map

--- frontend/src/component/GoogleMap/GoogleMap.js
+++ frontend/src/component/GoogleMap/GoogleMap.js
@@ -5,10 +5,15 @@ const GoogleMap = () => {
         <div className="map-page-container">
             <div className="map-outer-container">
                 <div className="store-list">
+                    <div className="find-store">
 
+                    </div>
+                    <div className="list">
+
+                    </div>
                 </div>
                 <div className="google-map">
-                    
+                    google map goes here
                 </div>
             </div>
         </div>
